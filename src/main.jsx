78afa333bec2df09561b764965568edae7a9a06a
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-07 08:32:13 -0500

    lesson 268

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { PokemonApp } from './PokemonApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 )
